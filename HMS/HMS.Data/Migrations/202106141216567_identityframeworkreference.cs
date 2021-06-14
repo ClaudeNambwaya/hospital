c13a6d0bb65fd2b28776d512df17d9c6599dad2d
ClaudeNambwaya <nambwaya92@gmail.com>
2021-06-14 15:37:36 +0300

    Added identity entity

---
+++ HMS/HMS.Data/Migrations/202106141216567_identityframeworkreference.cs
@@ -0,0 +1,18 @@
+﻿namespace HMS.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class identityframeworkreference : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "Discriminator", c => c.String(nullable: false, maxLength: 128));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "Discriminator");
+        }
+    }
+}
